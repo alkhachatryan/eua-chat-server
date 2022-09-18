c151b0f223dfcc155078d9df31405b3a32dc4810
Alexey <alexey.khachatryan@gmail.com>
2022-09-19 00:49:39 +0400

    Preparing to launch on heroku

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+})
 
 const HOST = process.env.APP_URL || 'http://localhost';
 const PORT = process.env.PORT || 3000;
